1799fe9fcd01aa0886f9a412e82f6fbf7d82a57d
YatagarasuGames <yatagarasugames5@gmail.com>
2025-07-26 18:35:05 +0300

    Initial

---
+++ Assets/PluginYourGames/Modules/Storage/Example/Scripts/SavesDataExample.cs
@@ -0,0 +1,10 @@
+namespace YG
+{
+    public partial class SavesYG
+    {
+        public float bestPointsRecord = 0;
+        public int intExample = 5;
+        public string strExample = "Hello!";
+        public bool[] boolExample = new bool[4];
+    }
+}
